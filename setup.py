7495858254858dea29d3fa85edec71eedb4c193b
flier.lu <flier.lu@b5a995e8-2231-c111-77cc-96630d28f406>
2010-08-31 07:59:32 +0000

    begin to work on v0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'asyncdns',
-    version = '0.2',
+    version = '0.3',
     packages = find_packages(exclude=['ez_setup', 'tests']),
     author = 'Flier Lu',
     author_email = 'flier.lu@gmail.com',
