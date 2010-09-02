71752cc5d3b5d39dc85e207618ca43275d4be9ae
flier.lu <flier.lu@b5a995e8-2231-c111-77cc-96630d28f406>
2010-09-02 03:55:06 +0000

    change the status to beta

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'dnspython >= 1.8.0',
     ],
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
